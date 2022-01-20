80c8d251010d438f77b3109088d79ce266b52116
loezasierra <joseloeza@gmail.com>
2022-01-19 21:03:27 -0800

    Added favicon

--- app/pages/designs/peach.js
+++ app/pages/designs/peach.js
@@ -1,9 +1,17 @@
+import Head from "next/head"
+
 import Layout from "../../components/layout"
+
 
 export default function Peach() {
     return (
         <Layout>
-            
+
+            <Head>
+                <title>Peach</title>
+                <link rel="icon" type="image/png" href="/peach/favicon.png" />
+            </Head>
+
         </Layout>
     )
 }
