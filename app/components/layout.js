c1c720c7e0501aefa302e381df84f11e8177844e
loezasierra <joseloeza@gmail.com>
2022-01-19 17:32:55 -0800

    Created a Layout component

---
+++ app/components/layout.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+
+export default function Layout ({ children, home }) {
+    return (
+        <div>
+
+            <Head>
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width,initial-scale=1" />
+            </Head>
+
+            {!home && (
+                <div>
+                    <Link href="/">
+                        <a>Back to home</a>
+                    </Link>
+                </div>
+            )}
+
+            <main>{children}</main>
+
+        </div>
+    )
+}
