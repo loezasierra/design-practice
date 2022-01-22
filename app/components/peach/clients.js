aaf45ea98bfe215ea01abc817a8ff43b3f2dc61b
loezasierra <joseloeza@gmail.com>
2022-01-22 13:07:20 -0800

    Changed 'id' to 'key'

--- app/components/peach/clients.js
+++ app/components/peach/clients.js
@@ -5,7 +5,7 @@ export default function Clients({ images }) {
     return (
         <>
             {images.map(({ id, filePath }) => (
-                <Image id={id} src={filePath} height="200" width="200" />
+                <Image key={id} src={filePath} height="200" width="200" />
             ))}
         </>
     )
