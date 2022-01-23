f714a1c22a474466af6621cb6e7ab7d182724ccd
loezasierra <joseloeza@gmail.com>
2022-01-22 17:47:36 -0800

    Added main styling sheet

--- app/pages/designs/peach.js
+++ app/pages/designs/peach.js
@@ -5,6 +5,7 @@ import Jumbotron from "../../components/peach/jumbotron"
 import LabeledSection from "../../components/peach/labeled-section"
 import Clients from "../../components/peach/clients"
 import Button from "../../components/peach/button"
+import styles from "../../components/peach/styles/peach.module.css"
 
 import { getImagePathsAndIDs } from "../../lib/publicFiles"
 import { getPagePathsAndIDs } from "../../lib/pageFiles"
@@ -12,36 +13,40 @@ import { getPagePathsAndIDs } from "../../lib/pageFiles"
 
 export default function Peach({ clientImages, pages }) {
     return (
-        <Layout>
+        <div class={styles.main}>
 
-            <Head>
-                <title>Peach</title>
-                <link rel="icon" type="image/png" href="/peach/favicon.png" />
-            </Head>
+            <Layout>
 
-            <Nav linkGroup={pages} />
+                <Head>
+                    <title>Peach</title>
+                    <link rel="icon" type="image/png" href="/peach/favicon.png" />
+                </Head>
 
-            <Jumbotron 
-                title="What's the best fruit?"
-                btntxt="Eat Peach"
-                imgsrc="/peach/peaches/peach1.png"
-            />
+                <Nav linkGroup={pages} />
 
-            <Jumbotron 
-                title="Want to try the delicious nectar of the gods?"
-                btntxt="Try Peach"
-                imgsrc="/peach/peaches/peach2.png"
-            />
+                <Jumbotron 
+                    title="What's the best fruit?"
+                    btntxt="Eat Peach"
+                    imgsrc="/peach/peaches/peach1.png"
+                />
 
-            <LabeledSection title="Find a Peach at Your Local Market">
-                <Clients images={clientImages} />
-            </LabeledSection>
+                <Jumbotron 
+                    title="Want to try the delicious nectar of the gods?"
+                    btntxt="Try Peach"
+                    imgsrc="/peach/peaches/peach2.png"
+                />
 
-            <LabeledSection title="Join the Peach Machine!">
-                <Button text="Peach Jobs" />
-            </LabeledSection>
+                <LabeledSection title="Find a Peach at Your Local Market">
+                    <Clients images={clientImages} />
+                </LabeledSection>
 
-        </Layout>
+                <LabeledSection title="Join the Peach Machine!">
+                    <Button text="Peach Jobs" />
+                </LabeledSection>
+
+            </Layout>
+        
+        </div>
     )
 }
 
