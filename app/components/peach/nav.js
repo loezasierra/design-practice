9be2d1ff3b4117529186513159bd665f31628d7e
loezasierra <joseloeza@gmail.com>
2022-01-22 23:55:40 -0800

    Added basic nav styling

--- app/components/peach/nav.js
+++ app/components/peach/nav.js
@@ -2,10 +2,11 @@ import Link from "next/link"
 import Logo from "./logo"
 import NavLinks from "./nav-links"
 import NavLinksGroup from "./nav-links-group"
+import styles from "./styles/nav.module.css"
 
 export default function Nav(props) {
     return (
-        <div>
+        <div class={styles.nav} >
             <Logo />
             <NavLinks>
                 <Link href="/"><a>Home</a></Link>
