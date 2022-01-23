e3cca7382c4a07a12d3f316e23b89e3ed8b38ad0
loezasierra <joseloeza@gmail.com>
2022-01-22 17:43:32 -0800

    Added font links

--- app/components/layout.js
+++ app/components/layout.js
@@ -8,6 +8,12 @@ export default function Layout ({ children, home }) {
             <Head>
                 <meta charSet="UTF-8" />
                 <meta name="viewport" content="width=device-width,initial-scale=1" />
+
+                {/* Fonts */}
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700;900&display=swap" rel="stylesheet" />
             </Head>
 
             {!home && (
