858e55eec9e485bbd80560a2b73a84461850c65c
loezasierra <joseloeza@gmail.com>
2022-01-25 13:17:43 -0800

    Added menu text styling

--- app/components/peach/nav.js
+++ app/components/peach/nav.js
@@ -6,6 +6,7 @@ import Logo from "./logo"
 import NavLinks from "./nav-links"
 import NavLinksGroup from "./nav-links-group"
 import styles from "./styles/nav.module.css"
+import utils from "./styles/peach.module.css"
 
 export default function Nav(props) {
     const [menuVisibility, setMenuVisibility] = useState(0)
@@ -23,7 +24,7 @@ export default function Nav(props) {
                 <Image src="/peach/menu.svg" height="25" width="25"/>
             </button>
 
-            <div className={`${styles.menu} ${menuVisibility ? styles.visible : styles.hidden}`}>
+            <div className={`${styles.menu} ${menuVisibility ? styles.visible : styles.hidden} ${utils.textX2}`}>
 
                 <NavLinks>
                     <Link href="/"><a>Home</a></Link>
