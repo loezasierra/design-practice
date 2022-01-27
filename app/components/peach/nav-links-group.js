a7192641777bac05398a41f2f9dbfb48e0a2182f
loezasierra <joseloeza@gmail.com>
2022-01-27 08:12:17 -0800

    Added basic show/hide linkgroup

--- app/components/peach/nav-links-group.js
+++ app/components/peach/nav-links-group.js
@@ -1,12 +1,19 @@
+import { useState } from 'react'
+
 import Link from 'next/link'
 import styles from './styles/nav-links-group.module.css'
 
 
 export default function NavLinksGroup({ title, links }) {
+    const [groupVisibility, setGroupVisibility] = useState(0)
+    const toggleGroup = () => groupVisibility ? setGroupVisibility(0) : setGroupVisibility(1)
+    
     return (
         <div>
-            <text className={styles.title}>{title}</text>
-            <div>
+            <button className={styles.button} onClick={() => toggleGroup()}>
+                <text className={styles.title}>{title}</text>
+            </button>
+            <div className={`${groupVisibility ? styles.visible : styles.hidden}`}>
                 {links.map(({ id, href }) => (
                     <Link key={id} href={href}><a>{id}</a></Link>
                 ))}
